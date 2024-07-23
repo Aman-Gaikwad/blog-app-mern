0eaa17b3b3ae909f391b3fafca3a0a55555f9e37
Aman Gaikwad <124183649+Aman-Gaikwad@users.noreply.github.com>
2024-07-23 14:32:29 +0000

    pages-created

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import {
+  About,
+  Dashboard,
+  Home,
+  Projects,
+  Signin,
+  Signup,
+} from "./pages/pages.exporter.js";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}  />
+          <Route path="/about" element={<About />}  />
+          <Route path="/dashboard" element={<Dashboard />}  />
+          <Route path="/projects" element={<Projects />}  />
+          <Route path="/signin" element={<Signin />}  />
+          <Route path="/signup" element={<Signup />}  />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/pages.exporter.js
@@ -0,0 +1,9 @@
+import About from './About.jsx'
+import Dashboard from './Dashboard'
+import Home from './Home.jsx'
+import Projects from './Projects.jsx'
+import Signin from './Signin.jsx'
+import Signup from './Signup.jsx'
+
+
+export {About,Dashboard,Home,Projects,Signin,Signup};
