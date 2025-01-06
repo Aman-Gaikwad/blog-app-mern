736c1b2979919eb09ed781f542df0775683e3f16
Aman Gaikwad <amangaikwad2018@gmail.com>
2025-01-07 01:33:37 +0530

    updating config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,21 +6,13 @@ export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-<<<<<<< HEAD
     flowbite.content(),
-=======
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}', // Spread the content array instead of calling the function
->>>>>>> 2d8cafffed25444cbd254b2c8c2af47a6b2475ed
   ],
   theme: {
     extend: {},
   },
   plugins: [
     flowbite.plugin(),
-<<<<<<< HEAD
     scrollbar,
-=======
-    scrollbar, // Use the imported plugin directly
->>>>>>> 2d8cafffed25444cbd254b2c8c2af47a6b2475ed
   ],
 };
